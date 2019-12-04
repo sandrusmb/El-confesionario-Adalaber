29d03ba731794b0798eede1e09295ffe5cd8eb96
sandrusmb <sandrusmb@gmail.com>
2019-12-04 01:42:10 +0100

    Add js and responsive design

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -13,19 +13,49 @@ const data = [
   "¿Cuánto sabías hace un mes? Pues eso."
 ];
 
-const button = document.querySelector(".container__form__button");
+console.log(data.length);
+
 const section = document.querySelector(".container__section");
 
-function paintAdvice(data) {}
+function paintPhrase(phrase) {
+  const h2 = document.createElement("h2");
+  h2.classList.add("container__section__text");
+  h2.innerHTML = phrase;
+  section.appendChild(h2);
+}
 
-function clickButtonHandler(data) {
-  for (let i = 0; i < data.length; i++) {
-    data[i];
-  }
-  if ((text.hidden = false)) {
-    section.classList.add("js-text");
+function deletePhrase() {
+  section.innerHTML = "";
+}
+
+//paintPhrase(data[3]);
+
+//función que retorna un número que puede ser los índices de mi array
+
+function getRandomPhrase() {
+  const index = getRandomInt(data.length);
+  const phrase = data[index];
+  return phrase;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+console.log(getRandomInt(10));
+
+const button = document.querySelector(".container__form__button");
+
+const input = document.querySelector(".container__form__input");
+
+function clickButtonHandler(event) {
+  event.preventDefault();
+  deletePhrase();
+  if (input.value !== "") {
+    const phrase = getRandomPhrase();
+    paintPhrase(phrase);
+    input.value = "";
   } else {
-    section.classList.remove("js-text");
+    alert("No seas tímida ¡cuéntanos qué te aflige!");
   }
 }
 
